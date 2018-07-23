de46043772f29c1197ad76c73849e224e3469892
fabio-gariglio <garigliofabio@hotmail.com>
2018-07-24 00:23:20 +0100

    Add Wizard and Worrior aggregates

---
+++ src/HRSaga/UnknownContext/DomainEvents/WizardCreatedDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using EventSourcing;
+
+namespace HRSaga.UnknownContext.DomainEvents
+{
+    public class WizardCreatedDomainEvent : IEvent
+    {
+        public string WizardId { get; set; }
+        public string WizardName { get; set; }
+    }
+}
